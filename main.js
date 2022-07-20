7266e76dc4d075c7ca7c8df03b0a462924d8f1c6
Sani-am <79805957+Sani-am@users.noreply.github.com>
2022-07-19 20:48:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createcanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('lipstickFilter.png');
+}
